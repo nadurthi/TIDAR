bf7726f4cafe3fee731c5c63befc8b5a98edaeb5
nagavenkat adurthi <n.adurthi@gmail.com>
2022-10-14 00:09:09 -0500

    good setup working

---
+++ camerahardware/calib_codes.py
@@ -0,0 +1,97 @@
+import cv2
+import numpy as np
+import pickle as pkl
+
+def calibrate_Cameras(Limg_o,ho,wo,imgpoints,object_points):
+
+
+    idxs = set(imgpoints[0].keys()) & set(imgpoints[1].keys()) & set(imgpoints[2].keys())
+    objpointsL_list=[]
+    imgpointsL_list_o=[]
+    imgpointsM_list_o=[]
+    imgpointsR_list_o=[]
+    for i in idxs:
+        if i in imgpoints[0] and i in imgpoints[1] and i in imgpoints[2]:
+            imgpointsL_list_o.append(imgpoints[0][i].astype(np.float32))
+            imgpointsM_list_o.append(imgpoints[1][i].astype(np.float32))
+            imgpointsR_list_o.append(imgpoints[2][i].astype(np.float32))
+            objpointsL_list.append(object_points)
+
+
+    calib_parameters={}
+    for scale in range(1,4):
+        print("scale = ",scale)
+        h=int(ho/scale)
+        w=int(wo/scale)
+        calib_parameters[scale]={'size':(h,w),'scale':scale}
+        Limg =cv2.resize(Limg_o,None,fx=1/scale,fy=1/scale,interpolation=cv2.INTER_AREA)
+        imgpointsL_list=[a/scale for a in imgpointsL_list_o]
+        imgpointsM_list = [a / scale for a in imgpointsM_list_o]
+        imgpointsR_list = [a / scale for a in imgpointsR_list_o]
+
+        imgpoints_set_list = [imgpointsL_list, imgpointsM_list, imgpointsR_list]
+
+        Lret, Lmtx, Ldist, Lrvecs, Ltvecs = cv2.calibrateCamera(objpointsL_list, imgpointsL_list, (w, h), None, None)
+        Lnewcameramtx, Lroi = cv2.getOptimalNewCameraMatrix(Lmtx, Ldist, (w, h), 1, (w, h))
+
+        Mret, Mmtx, Mdist, Mrvecs, Mtvecs = cv2.calibrateCamera(objpointsL_list, imgpointsM_list, (w, h), None, None)
+        Mnewcameramtx, Mroi = cv2.getOptimalNewCameraMatrix(Mmtx, Mdist, (w, h), 1, (w, h))
+
+        Rret, Rmtx, Rdist, Rrvecs, Rtvecs = cv2.calibrateCamera(objpointsL_list, imgpointsR_list, (w, h), None, None)
+        Rnewcameramtx, Rroi = cv2.getOptimalNewCameraMatrix(Rmtx, Rdist, (w, h), 1, (w, h))
+
+        calib_parameters[scale]['mono'] = {0: [Lret, Lmtx, Ldist, Lrvecs, Ltvecs, Lnewcameramtx, Lroi],
+                                    1: [Mret, Mmtx, Mdist, Mrvecs, Mtvecs, Mnewcameramtx, Mroi],
+                                    2: [Rret, Rmtx, Rdist, Rrvecs, Rtvecs, Rnewcameramtx, Rroi],
+                                    }
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # colors = ['r', 'g', 'b']
+        # for c in range(3):
+        #     rvecs = calib_parameters[scale]['mono'][c][3]
+        #     tvecs = calib_parameters[scale]['mono'][c][4]
+        #     for i in range(len(rvecs)):
+        #         Rmat, _ = cv2.Rodrigues(rvecs[i])
+        #         H = np.identity(4)
+        #         H[0:3, 0:3] = Rmat
+        #         H[0:3, 3] = tvecs[i].reshape(-1)
+        #         campos = np.linalg.inv(H)[0:3, 3]
+        #         ax.plot(campos[0], campos[1], campos[2], colors[c] + 'o')
+        #
+        # ax.plot(objpointsL_list[0][:, 0], objpointsL_list[0][:, 1], objpointsL_list[0][:, 2], 'ko')
+
+
+        # %% stereo calibrate
+        h, w = Limg.shape[:2]
+        calib_parameters[scale]['stereo'] = {}
+        calib_parameters[scale]['stereo_rect'] = {}
+        for c1, c2 in [(0, 2), (0, 1),(1,0),(1,2)]:
+
+            imgpt1 = imgpoints_set_list[c1]
+            imgpt2 = imgpoints_set_list[c2]
+
+            mtx1 = calib_parameters[scale]['mono'][c1][1]
+            dist1 = calib_parameters[scale]['mono'][c1][2]
+
+            mtx2 = calib_parameters[scale]['mono'][c2][1]
+            dist2 = calib_parameters[scale]['mono'][c2][2]
+
+            criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 3000, 0.000001)
+            # ,flags=cv2.CALIB_FIX_INTRINSIC+cv2.CALIB_USE_EXTRINSIC_GUESS
+            retval, cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F = cv2.stereoCalibrate(objpointsL_list,
+                                                                                                            imgpt1, imgpt2,
+                                                                                                            mtx1, dist1,
+                                                                                                            mtx2, dist2,
+                                                                                                            (w, h),
+                                                                                                            criteria=criteria)
+            calib_parameters[scale]['stereo'][(c1, c2)] = [cameraMatrix1, distCoeffs1, cameraMatrix2, distCoeffs2, R, T, E, F]
+
+            R1, R2, P1, P2, Q, validPixROI1, validPixROI2 = cv2.stereoRectify(cameraMatrix1, distCoeffs1, cameraMatrix2,
+                                                                             distCoeffs2, (w, h), R, T)
+            mapx1, mapy1 = cv2.initUndistortRectifyMap(cameraMatrix1, distCoeffs1, R1, P1[0:3, 0:3], (w, h), cv2.CV_32FC2)
+            mapx2, mapy2 = cv2.initUndistortRectifyMap(cameraMatrix2, distCoeffs2, R2, P2[0:3, 0:3], (w, h), cv2.CV_32FC2)
+
+            calib_parameters[scale]['stereo_rect'][(c1, c2)] = [R1, R2, P1, P2, Q, validPixROI1, validPixROI2, (mapx1, mapy1),
+                                                         (mapx2, mapy2)]
+
+    return calib_parameters
--- main_stereo_run.py
+++ main_stereo_run.py
@@ -38,24 +38,40 @@ from main_helpers import *
 import stereocodes.SGMpython.gpu_library  as gpu_library
 
 
+c1=0
+c2=2
+scale=1
+
 
 #%% testing calibrated stereo projection
 with open('multicam_config.json','r') as F:
     config_dict = json.load(F)
 
-
 with open("calibrated_camera_parameters.pkl","rb") as F:
     calib_parameters = pkl.load(F)
+
+config_dict['cameras']['height']=calib_parameters[scale]['size'][0]
+config_dict['cameras']['width']=calib_parameters[scale]['size'][1]
+
+
     
 config_dict['libSGM']["P1"]= 5
 config_dict['libSGM']["P2"]= 20
-config_dict['libSGM']["max_disparity"]= 128
-config_dict['libSGM']["min_disparity"]= 0
+config_dict['libSGM']["max_disparity"]= 256
+config_dict['libSGM']["min_disparity"]= 16
 config_dict['libSGM']["uniqueness"]= 0.98
 config_dict['libSGM']["num_paths"]= 8
 config_dict['libSGM']["LR_max_diff"]= 2
 
-hsmmodel = getmodel.getevalmodel(max_disp=384)
+config_dict['opencv_stereo']["P1"]= 5
+config_dict['opencv_stereo']["P2"]= 20
+config_dict['opencv_stereo']["max_disparity"]= 256
+config_dict['opencv_stereo']["min_disparity"]= 16
+config_dict['opencv_stereo']["uniquenessRatio"]= 5
+
+
+
+# hsmmodel = getmodel.getevalmodel(max_disp=384)
 
 # SGM models
 libsgm = gpu_library.Algo_libsgm(json.dumps(config_dict))
@@ -70,9 +86,9 @@ mTis.start_cameras()
 
 #%% configure target detection
 
-yolomodel = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
-yolomodel.cuda()
-yolomodel.eval()
+# yolomodel = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
+# yolomodel.cuda()
+# yolomodel.eval()
 
 
 #%% run simulation
@@ -83,13 +99,41 @@ for i in range(len(mTis)):
     cv2.namedWindow('Camera_%d' % i, cv2.WINDOW_NORMAL)
 cv2.namedWindow('Depth', cv2.WINDOW_NORMAL)
 
-c1=0
-c2=1
-(Lmapx, Lmapy), (Rmapx, Rmapy) = calib_parameters['stereo_rect'][(c1, c2)][7:9]
+class StereoParameterSetter:
+    def __init__(self):
+        self.libsgm = gpu_library.Algo_libsgm(json.dumps(config_dict))
+        self.libopencv = gpu_library.Algo_openCV(json.dumps(config_dict))
 
+    def on_trackbar_P1(self,val):
+        config_dict['opencv_stereo']["P1"]=val
+        config_dict['libSGM']["P1"] = val
+
+        print(val)
+        self.libsgm = gpu_library.Algo_libsgm(json.dumps(config_dict))
+        self.libopencv = gpu_library.Algo_openCV(json.dumps(config_dict))
+    def on_trackbar_P2(self,val):
+        config_dict['opencv_stereo']["P2"]=val
+        config_dict['libSGM']["P2"] = val
+        print(val)
+        self.libsgm = gpu_library.Algo_libsgm(json.dumps(config_dict))
+        self.libopencv = gpu_library.Algo_openCV(json.dumps(config_dict))
+
+sps=StereoParameterSetter()
+
+cv2.createTrackbar("P1", 'Depth' , 5, 300, sps.on_trackbar_P1)
+cv2.createTrackbar("P2", 'Depth' , 15, 500, sps.on_trackbar_P2)
+
+(Lmapx, Lmapy), (Rmapx, Rmapy) = calib_parameters[scale]['stereo_rect'][(c1, c2)][7:9]
+
+Lmapx, Lmapy = cv2.convertMaps(Lmapx, Lmapy, dstmap1type=cv2.CV_16SC2)
+Rmapx, Rmapy = cv2.convertMaps(Rmapx, Rmapy, dstmap1type=cv2.CV_16SC2)
 while 1:
     images = mTis.snapImages()
-        
+
+    if scale>1:
+        for i in range(len(mTis)):
+            images[i] = cv2.resize(images[i], None, fx=1 / scale, fy=1 / scale, interpolation=cv2.INTER_AREA)
+
     if images is None:
         continue
 
@@ -106,50 +150,53 @@ while 1:
     print("gray conversion = ",et-st)
 
     # deep
-    st = time.time()
-    disp_hsm = getmodel.runmodel(hsmmodel, Ldst.astype('float32'), Rdst.astype('float32'))
-    et=time.time()
-
-
-
-
-    # disp_csgm_libopencv=libopencv.compute_stereo_csgm(Ldstgray,Rdstgray)
-    # disp_csgm_libopencv=disp_csgm_libopencv/16
-    # disp_csgm_libopencv[disp_csgm_libopencv<=0]=0
-
-    # st=time.time()
-    # disp_libsgm_gpu = libsgm.getDisparity_gpu(Ldstgray,Rdstgray)
+    # st = time.time()
+    # disp = getmodel.runmodel(hsmmodel, Ldst.astype('float32'), Rdst.astype('float32'))
     # et=time.time()
-    # print("libsgm time = ",et-st)
 
-    Q = calib_parameters['stereo_rect'][(c1, c2)][4]
-    depths, depthcolor, pcd = get_colored_depth(Ldst, disp_hsm, Q, dmax=100, returnpcd=False)
+
+
+
+    # disp=sps.libopencv.compute_stereo_csgm(Ldstgray,Rdstgray)
+    # disp=disp/16
+    # disp[disp<=0]=0
+
+    st=time.time()
+    disp = sps.libsgm.getDisparity_gpu(Ldstgray,Rdstgray)
+    et=time.time()
+    print("libsgm time = ",et-st)
+
+    Q = calib_parameters[scale]['stereo_rect'][(c1, c2)][4]
+    depths, depthcolor, pcd = get_colored_depth(Ldst, disp, Q, dmax=100, returnpcd=False)
 
 
     #% target detection---------------------------------
-    st=time.time()
-    predictions = yolomodel([cv2.resize(Ldst, None,fx=0.5, fy=0.5,interpolation = cv2.INTER_AREA) ])
-    preds = []
-    for dd in predictions.pandas().xyxy:
-        b={'boxes': 2*dd[['xmin', 'ymin', 'xmax', 'ymax']].values.astype(int),
-                            'scores': dd['confidence'].values.astype(np.float32),
-                            'labels': dd['name'].values}
-        preds.append(b)
-
-    torch.cuda.empty_cache()
-
-
-    for j in range(len(preds)):
-        if j>0:
-            break
-        boxes_f = preds[j]['boxes']
-        labels_f = preds[j]['labels']
-        scores_f = preds[j]['scores']
-        for i in range(len(boxes_f)):
-            d = depths[boxes_f[i][1]:boxes_f[i][3],boxes_f[i][0]:boxes_f[i][2]].reshape(-1)
-            d=np.round(d[np.isfinite(d)],2)
-            cv2.rectangle(Ldst,(boxes_f[i][0],boxes_f[i][1]),(boxes_f[i][2],boxes_f[i][3]),(0,255,0),2)
-            cv2.putText(Ldst,str(labels_f[i])+" : "+str(scores_f[i])+" || {0:.2f},{0:.2f},{0:.2f}".format(np.min(d),np.mean(d),np.max(d)),(boxes_f[i][0],boxes_f[i][1]-10),0,1,(0,255,0),2)
+    # st=time.time()
+    # predictions = yolomodel([cv2.resize(Ldst, None,fx=0.5, fy=0.5,interpolation = cv2.INTER_AREA) ])
+    # preds = []
+    # for dd in predictions.pandas().xyxy:
+    #     b={'boxes': 2*dd[['xmin', 'ymin', 'xmax', 'ymax']].values.astype(int),
+    #                         'scores': dd['confidence'].values.astype(np.float32),
+    #                         'labels': dd['name'].values}
+    #     preds.append(b)
+    # 
+    # torch.cuda.empty_cache()
+    # 
+    # 
+    # for j in range(len(preds)):
+    #     if j>0:
+    #         break
+    #     boxes_f = preds[j]['boxes']
+    #     labels_f = preds[j]['labels']
+    #     scores_f = preds[j]['scores']
+    #     for i in range(len(boxes_f)):
+    #         d = depths[boxes_f[i][1]:boxes_f[i][3],boxes_f[i][0]:boxes_f[i][2]].reshape(-1)
+    #         if len(d)==0:
+    #             d=[np.inf]
+    #         else:
+    #             d=np.round(d[np.isfinite(d)],2)
+    #         cv2.rectangle(Ldst,(boxes_f[i][0],boxes_f[i][1]),(boxes_f[i][2],boxes_f[i][3]),(0,255,0),2)
+    #         cv2.putText(Ldst,str(labels_f[i])+" : "+str(scores_f[i])+" || {0:.2f},{0:.2f},{0:.2f}".format(np.min(d),np.mean(d),np.max(d)),(boxes_f[i][0],boxes_f[i][1]-10),0,1,(0,255,0),2)
 
 
     et = time.time()
